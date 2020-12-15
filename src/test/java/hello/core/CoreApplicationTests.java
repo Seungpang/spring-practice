9ff37f21f97c02e47bdc45f01c15065988a20a99
Seungpang <obey1342@gmail.com>
2020-12-15 14:34:53 +0900

    회원 도메인 개발

--- src/test/java/hello/core/CoreApplicationTests.java
+++ src/test/java/hello/core/CoreApplicationTests.java
@@ -1,13 +1,13 @@
-package com.seungpang.webmvc;
+package hello.core;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WebmvcApplicationTests {
+class CoreApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
